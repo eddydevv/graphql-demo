ea4e3c86c21582c10543d97dba3e6b1696c868ac
Edwin Dela Kwesi Agbenyega <eddybanks@unm.edu>
2018-11-20 16:28:52 -0700

    exploring query and mutation types

--- index.js
+++ index.js
@@ -8,16 +8,36 @@ app.get('/', (req, res) => {
   res.send('GraphQL is amazing!')
 })
 
-const root = { friend: () => {
-  return {
-    "id": 12634564,
-    "firstName": "Edwin",
-    "lastName": "Agbenyega",
-    "gender": "Male",
-    "language": "English",
-    "email": "eddy.devv@gmail.com"
+class Friend {
+  constructor(id, {firstName, lastName, gender, language, email}) {
+    this.id = id
+    this.firstName = firstName
+    this.lastName = lastName
+    this.gender = gender
+    this.language = language
+    this.email = email
   }
-}}
+}
+
+const friendDatabase = {}
+
+const root = { 
+  friend: () => {
+    return {
+      "id": 12634564,
+      "firstName": "Edwin",
+      "lastName": "Agbenyega",
+      "gender": "Male",
+      "language": "English",
+      "email": "eddy.devv@gmail.com"
+    }
+  },
+  createFriend: ({input}) => {
+    let id = require('crypto').randomBytes(10).toString('hex')
+    friendDatabase[id] = input
+    return new Friend(id, input)
+  }
+}
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
--- schema.js
+++ schema.js
@@ -2,17 +2,30 @@ import { buildSchema } from 'graphql'
 
 const schema = buildSchema(`
   type Friend {
-    id: ID,
-    firstName: String,
-    lastName: String, 
-    gender: String,
-    language: String,
+    id: ID
+    firstName: String
+    lastName: String 
+    gender: String
+    language: String
     email: String
   }
 
   type Query {
     friend: Friend
   }  
+
+  input FriendInput {
+    id: ID
+    firstName: String!
+    lastName: String 
+    gender: String
+    language: String
+    email: String
+  }
+
+  type Mutation {
+    createFriend(input: FriendInput): Friend
+  }
 `)
 
 export default schema
