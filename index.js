dcf380947e1167c817c7e1f4a5c6fa24d7facd0d
Edwin Dela Kwesi Agbenyega <eddybanks@unm.edu>
2018-11-20 17:09:47 -0700

    refactoring with graphql tools but with few errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
-import schema from './schema'
+import { schema } from './schema'
 
 const app = express()
 
@@ -8,41 +8,9 @@ app.get('/', (req, res) => {
   res.send('GraphQL is amazing!')
 })
 
-class Friend {
-  constructor(id, {firstName, lastName, gender, language, email}) {
-    this.id = id
-    this.firstName = firstName
-    this.lastName = lastName
-    this.gender = gender
-    this.language = language
-    this.email = email
-  }
-}
-
-const friendDatabase = {}
-
-const root = { 
-  friend: () => {
-    return {
-      "id": 12634564,
-      "firstName": "Edwin",
-      "lastName": "Agbenyega",
-      "gender": "Male",
-      "language": "English",
-      "email": "eddy.devv@gmail.com"
-    }
-  },
-  createFriend: ({input}) => {
-    let id = require('crypto').randomBytes(10).toString('hex')
-    friendDatabase[id] = input
-    return new Friend(id, input)
-  }
-}
-
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
-  graphiql: true
+  graphiql: true,
 }))
 
 app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'))
